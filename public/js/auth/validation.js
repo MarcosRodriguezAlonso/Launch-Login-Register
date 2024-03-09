3c4c8b42caee1804c5f7402f5b9e60595e22b230
Marcos Rodríguez <e.marcosra@gmail.com>
2024-03-09 15:13:37 +0100

    Correcciones para la entrega 09/03/2024

--- public/js/auth/validation.js
+++ public/js/auth/validation.js
@@ -1,6 +1,7 @@
 export const isValidUsername = (username) => {
   let isValid;
-  if (username.length >= 5);
+  if (username.length >= 5) isValid = true;
+  else isValid = false;
   return isValid;
 };
 
@@ -10,18 +11,22 @@ export const isValidPassword = (password) => {
     /.{8,}/.test(password) &&
     /(?:[A-Z])/.test(password) &&
     /(?:\d)/.test(password)
-  );
+  )
+    isValid = true;
+  else isValid = false;
   return isValid;
 };
 
 export const isSamePassword = (password1, password2) => {
   let isSame;
-  if (password1 === password2);
+  if (password1 === password2) isSame = true;
+  else isSame = false;
   return isSame;
 };
 
 export const isAdultAge = (age) => {
   let isAdult;
-  if (age >= 18);
+  if (age >= 18) isAdult = true;
+  else isAdult = false;
   return isAdult;
 };
