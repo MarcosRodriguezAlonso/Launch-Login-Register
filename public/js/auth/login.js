7be85d1c7e327002177ec07dbbcf7249ca132cd7
Marcos Rodríguez <e.marcosra@gmail.com>
2024-03-11 19:14:13 +0100

    fix "if" syntax at login.js & validation.js

--- public/js/auth/login.js
+++ public/js/auth/login.js
@@ -2,9 +2,7 @@ export const checkCredentials = (username, password) => {
   const storedUsername = localStorage.getItem("username");
   const storedPassword = localStorage.getItem("password");
 
-  let areValidCredentials;
-  if (username === storedUsername && password === storedPassword)
-    areValidCredentials = true;
-  else areValidCredentials = false;
+  const areValidCredentials =
+    username === storedUsername && storedPassword === password;
   return areValidCredentials;
 };
--- public/js/auth/validation.js
+++ public/js/auth/validation.js
@@ -1,32 +1,48 @@
 export const isValidUsername = (username) => {
   let isValid;
-  if (username.length >= 5) isValid = true;
-  else isValid = false;
+  if (username.length >= 5) {
+    isValid = true;
+  } else {
+    isValid = false;
+  }
+
   return isValid;
 };
 
 export const isValidPassword = (password) => {
   let isValid;
   if (
-    /.{8,}/.test(password) &&
-    /(?:[A-Z])/.test(password) &&
-    /(?:\d)/.test(password)
-  )
+    /([ÁÉÍÓÚáéíóúÑñ])/.test(password) &&
+    (password.length = 8) &&
+    /([A-Z])/.test(password) &&
+    /(\d)/.test(password)
+  ) {
     isValid = true;
-  else isValid = false;
+  } else {
+    isValid = false;
+  }
+
   return isValid;
 };
 
 export const isSamePassword = (password1, password2) => {
   let isSame;
-  if (password1 === password2) isSame = true;
-  else isSame = false;
+  if (password1 === password2) {
+    isSame = true;
+  } else {
+    isSame = false;
+  }
+
   return isSame;
 };
 
 export const isAdultAge = (age) => {
   let isAdult;
-  if (age >= 18) isAdult = true;
-  else isAdult = false;
+  if (age >= 18) {
+    isAdult = true;
+  } else {
+    isAdult = false;
+  }
+
   return isAdult;
 };
