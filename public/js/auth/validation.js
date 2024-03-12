51e6e35d45bd79d44bde206b54c8b767a8e7cba7
Marcos Rodríguez <e.marcosra@gmail.com>
2024-03-12 14:01:24 +0100

    update after corrections

--- public/js/auth/validation.js
+++ public/js/auth/validation.js
@@ -1,48 +1,26 @@
 export const isValidUsername = (username) => {
-  let isValid;
-  if (username.length >= 5) {
-    isValid = true;
-  } else {
-    isValid = false;
-  }
+  const isValid = username.length >= 5;
 
   return isValid;
 };
 
 export const isValidPassword = (password) => {
-  let isValid;
-  if (
-    /([ÁÉÍÓÚáéíóúÑñ])/.test(password) &&
+  const isValid =
+    password.toLowerCase() !== password &&
     (password.length = 8) &&
-    /([A-Z])/.test(password) &&
-    /(\d)/.test(password)
-  ) {
-    isValid = true;
-  } else {
-    isValid = false;
-  }
+    /(\d)/.test(password);
 
   return isValid;
 };
 
 export const isSamePassword = (password1, password2) => {
-  let isSame;
-  if (password1 === password2) {
-    isSame = true;
-  } else {
-    isSame = false;
-  }
+  const isSame = password1 === password2;
 
   return isSame;
 };
 
 export const isAdultAge = (age) => {
-  let isAdult;
-  if (age >= 18) {
-    isAdult = true;
-  } else {
-    isAdult = false;
-  }
+  const isAdult = age >= 18;
 
   return isAdult;
 };
